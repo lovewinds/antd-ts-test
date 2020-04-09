bac0ab32b0c888d22156c35189987e07d5879985
lovewinds <lovewinds.4i@gmail.com>
2020-04-09 17:15:22 +0900

    Add ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <Button type="primary">Learn React</Button>
         </a>
       </header>
     </div>
