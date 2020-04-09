1cf9304a5f05820ae9cf98efe62715a20a3beda8
lovewinds <lovewinds.4i@gmail.com>
2020-04-09 19:37:46 +0900

    Simple header / footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Button } from 'antd';
+import { Layout } from 'antd';
+import { Typography } from 'antd';
+
 import './App.css';
+
+const { Header, Footer, Content } = Layout;
+const { Title } = Typography;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button type="primary">Learn React</Button>
-        </a>
-      </header>
+      <Layout>
+        <Header className="Ant-Header">
+          <Title>Translate</Title>
+        </Header>
+        <Content>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button type="primary">Learn React</Button>
+            </a>
+          </header>
+        </Content>
+        <Footer className="Ant-Footer">
+          Sample © 2020 with Ant Design
+        </Footer>
+      </Layout>
+      
     </div>
   );
 }
